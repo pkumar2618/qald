fbd29d2316bc208f4278e35261179cd8ebde8dd4
Pawan Kumar <pawan.kumar@cse.iitd.ac.in>
2019-06-09 21:19:25 +0530

    pos-complexity, list type, and count type done

--- helper_fn.py
+++ helper_fn.py
@@ -1,8 +1,85 @@
 import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tag import pos_tag
+# from nltk.tokenize import word_tokenize
+# from nltk.tag import pos_tag
+import re
 
 def sentence_preprocessing(sentence):
     sentence = nltk.word_tokenize(sentence)
     sentence = nltk.pos_tag(sentence)
     return sentence
+
+def pos_complex(pos_tagged_sentence):
+    pattern_nn = ["NN.*", "WP"]
+    pattern_nn_combined = "(" + ")|(".join(pattern_nn) + ")"
+    # print(pattern)
+    noun_count = 0
+    other_count = 0
+    for word_tag in pos_tagged_sentence:
+        # noun_tag = re.search(r'(NN.*)|(WP)', word_tag[1])
+        noun_tag = re.search(pattern_nn_combined, word_tag[1])
+        if noun_tag:
+            noun_count += 1
+        else:
+            other_count += 1
+
+    if noun_count > 3 and other_count > 0:
+        return "hard"
+    elif noun_count == 3 and other_count > 0:
+        return "medium"
+    else:
+        return "easy"
+
+
+def list_type(sentence):
+    list_count = 0
+    for word_tag in sentence:
+        list_found = re.search(r'\b[lL]ist\b', word_tag[0])
+        if list_found:
+            list_count += 1
+
+    for i in range(len(sentence) - 3):
+        if re.search(r'\b([gG]ive)|([fF]ind)\b', sentence[i][0]):
+            if (re.search(r'\b(me)\b', sentence[i+1][0])) and (re.search(r'\ball\b', sentence[i+2][0])):
+                list_count += 1
+            elif (re.search(r'(\ball\b)', sentence[i+1][0])) and (re.search(r'\bthe\b', sentence[i+2][0])):
+                list_count += 1
+
+    if list_count >= 1:
+        return "Y"
+    else:
+        return "N"
+
+
+def count_type(sentence):
+    count_count = 0
+    for word_tag in sentence:
+        count_found = re.search(r'\b[cC]ount[sS]*\b', word_tag[0])
+        if count_found:
+            count_count += 1
+
+    if count_count >= 1:
+        return "Y"
+    else:
+        return "N"
+
+
+def ner_complex(sentence):
+    pattern_nn = ["NN.*", "WP"]
+    pattern_nn_combined = "(" + ")|(".join(pattern_nn) + ")"
+    # print(pattern)
+    noun_count = 0
+    other_count = 0
+    for word_tag in pos_tagged_sentence:
+        # noun_tag = re.search(r'(NN.*)|(WP)', word_tag[1])
+        noun_tag = re.search(pattern_nn_combined, word_tag[1])
+        if noun_tag:
+            noun_count += 1
+        else:
+            other_count += 1
+
+    if noun_count > 3 and other_count > 0:
+        return "hard"
+    elif noun_count == 3 and other_count > 0:
+        return "medium"
+    else:
+        return "easy"
--- ner_complex.py
+++ ner_complex.py
@@ -2,25 +2,77 @@ import json
 import pandas as pd
 import nltk
 import pickle
-from helper_fn import sentence_preprocessing
+import numpy as np
+from helper_fn import sentence_preprocessing, pos_complex, list_type, count_type
 
+
+"""
+creating pandas dataframe with qald id, sentence, pos-complex, ner-complex,
+OpenIE-complex, list type(Y/N), count_type(Y/N)
+"""
 # dict_multilingual_question = json.load(open("qald-9-train-multilingual.json"))
-#
-# ### creating pandas dataframe with qald id, sentence, pos-complex, ner-complex, OpenIE-complex, list type(Y/N), count_type(Y/N)
 # category_df = pd.DataFrame(columns=["qald_id", "sentence_en", "pos-complex(H/M/E)", "ner-complex(H/M/E)",
 #                                        "OpenIE-complex(H/M/E)", "list_type(Y/N)", "count_type(Y/N)"])
 #
 # for item_obj in dict_multilingual_question["questions"]:
 #     for lang_obj in item_obj["question"]:
 #         if lang_obj["language"] == "en":
-#             category_df = category_df.append({'qald_id': item_obj["id"], 'sentence_en': lang_obj["string"]}, ignore_index=True).fillna(0)
-#
+#             category_df = category_df.append({'qald_id': item_obj["id"], 'sentence_en':
+#             lang_obj["string"]}, ignore_index=True).fillna("tbd")
 # # print(category_df)
 # pickle_handle = open("pickle_df_raw", "wb")
 # pickle.dump(category_df, pickle_handle)
 # pickle_handle.close()
 
-pickle_handle = open("pickle_df_raw", "rb")
+
+"""
+determining POS complexity
+pos-complex(E): when sentence has two or less NN/WP tags and 0+ not(NN) tag
+pos-complex(M): when sentence has 3 NN/WP tags and 1+ not(NN and WP) tags
+pos-complex(H): when sentence has 4 or more NN/WP tags and 1+ not(NN and WP) tags
+"""
+# pickle_handle = open("pickle_df_raw", "rb")
+# category_df = pickle.load(pickle_handle)
+# pickle_handle.close()
+# # # print(category_df)
+# category_df = category_df.set_index("qald_id", drop=False)
+# for id_label in category_df.index:
+#     sentence = sentence_preprocessing(category_df.at[id_label, "sentence_en"])
+#     # print(sentence)
+#     label_HME = pos_complex(sentence)
+#     category_df.at[id_label, "pos-complex(H/M/E)"] = label_HME
+#
+# pickle_handle = open("pickle_df_processed", "wb")
+# pickle.dump(category_df, pickle_handle)
+# pickle_handle.close()
+
+
+"""
+label sentence are 'list' type and 'count' type
+"""
+# pickle_handle = open("pickle_df_processed", "rb")
+# category_df = pickle.load(pickle_handle)
+# pickle_handle.close()
+# # print(category_df)
+# category_df = category_df.set_index("qald_id", drop=False)
+# for id_label in category_df.index:
+#     sentence = sentence_preprocessing(category_df.at[id_label, "sentence_en"])
+#     # print(sentence)
+#     list_found = list_type(sentence)
+#     category_df.at[id_label, "list_type(Y/N)"] = list_found
+#     count_found = count_type(sentence)
+#     category_df.at[id_label, "count_type(Y/N)"] = count_found
+#
+# pickle_handle = open("pickle_df_processed", "wb")
+# pickle.dump(category_df, pickle_handle)
+# pickle_handle.close()
+# print(category_df)
+
+
+"""
+NER Tagging
+"""
+pickle_handle = open("pickle_df_processed", "rb")
 category_df = pickle.load(pickle_handle)
 pickle_handle.close()
 print(category_df)
